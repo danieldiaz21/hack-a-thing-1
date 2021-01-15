55247d882458655dc2a1c502487ef39bf330aaad
Daniel Diaz <daniel.diaz.21@dartmouth.edu>
2021-01-14 16:58:35 -0800

    Created basic registration form

--- RegistrationForm/app/src/main/java/com/example/registrationform/MainActivity.java
+++ RegistrationForm/app/src/main/java/com/example/registrationform/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.registrationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onRegisterBtnClick (View view) {
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        EditText edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
+        EditText edtTxtLastName = findViewById(R.id.edtTextLastName);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        txtFirstName.setText("First Name: " + edtTxtFirstName.getText().toString());
+        txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString());
+        txtEmail.setText("Email: " + edtTxtEmail.getText().toString());
     }
 }
